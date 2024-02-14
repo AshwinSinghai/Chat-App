9407d15014b839ca6f767bd54f45c1da04aaba62
Ashwin Singhai <hs0301200220@gmail.com>
2024-02-14 11:47:16 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlVn7jSyiJVP9nNuJhHY78QzLuOHzK7lM",
+  authDomain: "hashirachatapp.firebaseapp.com",
+  projectId: "hashirachatapp",
+  storageBucket: "hashirachatapp.appspot.com",
+  messagingSenderId: "235535886445",
+  appId: "1:235535886445:web:870040e8f7835fc1d17764",
+};
+
+export const app = initializeApp(firebaseConfig);
